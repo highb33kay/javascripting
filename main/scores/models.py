ae2e218dc7c594b2815707d94451285e147c601c
highb33kay <hiibeekayvibe@gmail.com>
2022-07-17 01:54:58 +0100

    rebulding my database

--- main/scores/models.py
+++ main/scores/models.py
@@ -1,3 +1,19 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Game(models.Model):
+    player = models.ForeignKey(
+        Player, verbose_name='player', on_delete=models.CASCADE)
+    wins = models.IntegerField(verbose_name='wins')
+    loss = models.IntegerField(verbose_name="loss")
+    points = models.IntegerField(verbose_name='points')
+    day_played = models.DateField(
+        default=date.date.today, verbose_name="date", blan
+    played=models.
