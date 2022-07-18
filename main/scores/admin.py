f89cb183b24073e88e93c06654bff01a8304995b
highb33kay <hiibeekayvibe@gmail.com>
2022-07-18 20:01:49 +0100

    added list display to admin

--- main/scores/admin.py
+++ main/scores/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Game, Player
 # Register your models here.
 
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("player", "wins", "loss", "points", "day_played")
+
+
 admin.site.register(Game)
 admin.site.register(Player)
